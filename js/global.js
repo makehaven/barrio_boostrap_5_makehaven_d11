84237bfbf5637ef425f1e4234d01dbe9a99ab4b3
J.R. Logan <johnrichardlogan@gmail.com>
2026-02-24 13:46:38 -0500

    Add dashboard styles and refine event page behavior

--- js/global.js
+++ js/global.js
@@ -3,14 +3,21 @@
  * Global utilities.
  *
  */
-(function (Drupal) {
+(function (Drupal, once) {
 
   'use strict';
 
   Drupal.behaviors.barrio_boostrap_5_makerspace = {
     attach: function (context, settings) {
-
+      once('mh-dashboard-sparkline', '.kpi-sparkline-wrapper svg', context).forEach(function (svg) {
+        const width = parseFloat(svg.getAttribute('width')) || 120;
+        const height = parseFloat(svg.getAttribute('height')) || 40;
+        if (!svg.getAttribute('viewBox')) {
+          svg.setAttribute('viewBox', `0 0 ${width} ${height}`);
+        }
+        svg.setAttribute('preserveAspectRatio', 'none');
+      });
     }
   };
 
-})(Drupal);
+})(Drupal, once);
